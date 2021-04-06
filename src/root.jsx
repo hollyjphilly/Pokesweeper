bce4c54db33af4d7103c5822a697866502cba1f5
hollyjphilly <61254925+hollyjphilly@users.noreply.github.com>
2021-04-06 00:13:08 -0400

    Testing Github Pages

--- src/root.jsx
+++ src/root.jsx
@@ -3,7 +3,9 @@ import Game from './game';
 
 const Root = (props) => {
     return (
-        <div><Game /></div>
+        <div>
+        <Game />
+        <h5>Shift click to flag a Voltorb. Flag all Voltorbs to win.</h5></div>
     )
 };
 
